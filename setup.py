fdc42c6e624928ea6ba9aee58bd551df6d56e90f
Sneaha-oss <snehabehera1616@gmail.com>
2026-08-20 01:09:05 +0530

    Change package name to 'OpenVoice' and update description
    Updated package name and description in setup.py.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 
-setup(name='MyShell-OpenVoice',
+setup(name='OpenVoice',
       version='0.0.0',
-      description='Instant voice cloning by MyShell.',
+      description='voice cloning .',
       long_description=open('README.md').read().strip(),
       long_description_content_type='text/markdown',
       keywords=[
@@ -12,16 +12,8 @@ setup(name='MyShell-OpenVoice',
             'voice-clone',
             'zero-shot-tts'
       ],
-      url='https://github.com/myshell-ai/OpenVoice',
-      project_urls={
-        'Documentation': 'https://github.com/myshell-ai/OpenVoice/blob/main/docs/USAGE.md',
-        'Changes': 'https://github.com/myshell-ai/OpenVoice/releases',
-        'Code': 'https://github.com/myshell-ai/OpenVoice',
-        'Issue tracker': 'https://github.com/myshell-ai/OpenVoice/issues',
       },
-      author='MyShell',
-      author_email='ethan@myshell.ai',
-      license='MIT License',
+
       packages=find_packages(),
 
       python_requires='>=3.9',
